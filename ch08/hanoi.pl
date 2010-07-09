1a0bf7be7421952d741505f9feec86c9da5e25c5
Hiromi Ishii <mr_konn@jcom.home.ne.jp>
2010-07-09 23:03:45 +0900

    from ch 09

--- ch08/hanoi.pl
+++ ch08/hanoi.pl
@@ -2,11 +2,5 @@ append([], Ys, Ys).
 append([X|Xs], Ys, [X|Zs]) :- append(Xs, Ys, Zs).
 
 hanoi(0, A, B, C, []).
-hanoi(N, A, B, C, Moves) :-
-	>(N, 0), 
-	is(M, N - 1), 
-	hanoi(M, A, C, B, Ms1),
-	hanoi(M, C, B, A, Ms2),
-	append(Ms1, [to(A, B)|Ms2], Moves).
-
+hanoi(N, A, B, C, Moves) :- >(N,0), is(M, -(N,1)), hanoi(M, A, C, B, Ms1), hanoi(M, C, B, A, Ms2), append(Ms1, [to(A, B)|Ms2], Moves).
 t(X) :- X.
